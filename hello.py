0f6a34c17adfe6278475bc7e9e46667afd0f7af2
arielreches <arielreches@gmail.com>
2016-08-07 17:53:19 -0400

    some bootstrap to get stahted

--- hello.py
+++ hello.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
 from flask import make_response
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 
 @app.route('/')
 def index():
 	politicians = ["trump", "hillary"]
-	return render_template('index.html', politicians=politicians)
+	return render_template('user.html')
 
 
 @app.route('/user/<name>')
